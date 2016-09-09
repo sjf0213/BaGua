da9ff921d4575a1ddcf3b20d7b956b373d06eb02
宋炬峰 <sjf0213@163.com>
2016-09-09 18:02:26 +0800

    prj setup

---
+++ BaGua/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaGua
+//
+//  Created by 宋炬峰 on 16/9/9.
+//  Copyright © 2016年 appfactory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
